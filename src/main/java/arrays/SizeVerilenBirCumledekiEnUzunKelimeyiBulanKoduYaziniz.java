298cf3c6b33d76ae714236f6b62b3781fd489e2b
hanzelkaraagac <hanzelgamess@gmail.com>
2022-10-23 02:07:25 +0300

    Arrays Sorular

---
+++ src/main/java/arrays/SizeVerilenBirCumledekiEnUzunKelimeyiBulanKoduYaziniz.java
@@ -0,0 +1,31 @@
+package arrays;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class SizeVerilenBirCumledekiEnUzunKelimeyiBulanKoduYaziniz {
+    public static void main(String[] args) {
+
+        //Example 3: Size verilen bir cumledeki en uzun kelimeyi bulan kodu yaziniz.
+        //           "Java kolaydir calisana, ne kolay ki calismayana."
+
+        String cumle = "Java kolaydir calisana, ne kolay ki calismayana.";
+        System.out.println(cumle);//Java kolaydir calisana, ne kolay ki calismayana.
+
+        cumle = cumle.replaceAll("\\p{Punct}", "");
+        System.out.println(cumle);//Java kolaydir calisana ne kolay ki calismayana
+
+        cumle.split(" ");
+
+        String kelimeler [] = cumle.split(" ");//[Java, kolaydir, calisana, ne, kolay, ki, calismayana]
+        System.out.println(Arrays.toString(kelimeler));
+
+        Arrays.sort(kelimeler, Comparator.comparingInt(String::length));//[ne, ki, Java, kolay, kolaydir, calisana, calismayana]
+        System.out.println(Arrays.toString(kelimeler));
+
+        System.out.println(kelimeler [kelimeler.length-1]);//calismayana
+
+
+
+    }
+}
